36b7226aae87fbe9f8119a18c35fa18ac7c372c3
SatharakaNilmantha <satharakanilmantha1@gmail.com>
2026-01-16 10:51:47 +0530

    connect  product page with backend

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -8,6 +8,8 @@ import logo from '../../Images/logo/logoRemovebgwhite.png';
 import ProductPage from "./ProductPage/ProductPage.jsx";
 
 
+
+
 function AdminPage() {
   
   const [open, setOpen] = useState(false);
